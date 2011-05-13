617a948827aab44ab55736738f627219cf75b9a2
Swaroop Ramaswamy <swaroop551992@gmail.com>
2011-05-13 13:26:41 +0530

    Added two more fields to Task

--- schedule/models.py
+++ schedule/models.py
@@ -4,10 +4,13 @@ from django.db import models
 
 class Task(models.Model):
 
-    task_subject     = models.CharField(max_length=100 , null=True , blank=True)
-    task_description = models.TextField(null=True , blank=True)
-    task_creator     = models.ForeignKey(User , related_name = "tasks_assigned")
-    task_assigned_to = models.ManyToManyField(User, related_name = "tasks")
+    task_subject      = models.CharField(max_length=100 , null=True , blank=True)
+    task_description  = models.TextField(null=True , blank=True)
+    task_creator      = models.ForeignKey(User , related_name = "tasks_assigned")
+    task_assigned_to  = models.ManyToManyField(User, related_name = "tasks")
     #Task can be assigned to multiple people, say all webops coords etc. Think about how we can implement this by grouping coords etc
-    task_deadline    = models.DateTimeField(null=True , blank=True)
+    task_deadline     = models.DateTimeField(null=True , blank=True)
+    task_status       = models.TextField(null=True , blank =True)
+    flag_completed    = models.BooleanField(default=False)
+    #False - not completed, True - completed
     #Note that there must be a thread associated with each task 
