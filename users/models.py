36b062d47841a3b972feb65e4cce3e01c6b088d5
Krishna <krishna@Krishna-sonata.(none)>
2011-05-13 14:22:47 +0530

    Wrote the Basic User-Profile Model

---
+++ users/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib import admin
+
+# Create your models here.
+
+GENDER_CHOICES = (
+    ('M','Male'),
+    ('F','Female'),
+)
+#List of Department Choices
+DEP_CHOICES = (
+	("Events", "Events"),
+	("QMS", "Quality Management"),
+	("Finance", "Finance"),
+	("Sponsorship", "Sponsorship"),
+	("Evolve", "Evolve"),
+	("Facilities", "Facilities"),
+	("Webops", "Web Operations"),
+	("Hospitality", "Hospitality"),
+	("Publicity", "Publicity"),
+	("Design", "Design"),
+)
+
+#This is the initial users model
+#Author-Krishna Shrinivas
+class userprofile(models.Model):
+    user= models.ForeignKey(User, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=1,choices=GENDER_CHOICES,default='M')
+    age = models.IntegerField(default=18,)
+    department_belong = models.CharField(max_length=50,choices=DEP_CHOICES,default='Events')
+    #This is for QMS co-ords as they will monitor a different department,for others it is the same department
+    department_monitor=models.CharField(max_length=50,choices=DEP_CHOICES,default='Events')
+    mobile_number = models.CharField(max_length=15)
+    college_roll = models.CharField(max_length=40,default='Enter College Id/Roll No.')
+    activation_key = models.CharField(max_length=40)
+    key_expires = models.DateTimeField()
+    is_core=models.BooleanField(default=False,blank=True)
+    is_coord=models.BooleanField(default=False,blank=True)
+   #i Havent written the methods as yet, do we use them as methods in a class or in views?
+  def __str__(self):
+
+        return self.user.username
+
+    class Admin:
+        pass
+
