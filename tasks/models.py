a1d60fb0a75f6163dfbe5a0ff1816421379be0b8
S Pradeep Kumar <gohanpra@gmail.com>
2011-05-22 17:19:27 +0530

    Definition of Task Models for Alpha version done.

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from erp.department.models import *
+from erp.department.models import Department
+
 # Create your models here.
 #The choices may be cup level but if any thing better pls do change.
 STAT_CHOICES= (
@@ -9,32 +10,80 @@ STAT_CHOICES= (
 	('L','Overdue'),
 	('N','Almost'),
 )
-class Task(models.Model):
 
-    subject        = models.CharField(max_length=100 , null=True , blank=True)
-    description    = models.TextField(null=True , blank=True)
-    creator        = models.ForeignKey(User , related_name = "tasks_assigned")
-    creation_date  = models.DateTimeField (auto_now = True, editable = False)
-    assignee    = models.ManyToManyField(User, related_name = "tasks")
-    deadline       = models.DateTimeField(null=True , blank=True)
-    status         = models.TextField(max_length=50,choices=STAT_CHOICES,default='OPEN')	
-    department=models.ManyToManyField(Department, related_name = "tasks_dept")
-    manager=models.ManyToManyField(User,related_name="tasks_monitored")
+class AbstractBaseTask(models.Models):
+    """ Abstract Base Class for Task, SubTask.
+
+    TODO:
+    * File upload
+    * Draft saving
+    * QMS Manager for the Task
+    """
+    subject       = models.CharField(max_length=100 , null=True , blank=True)
+    description   = models.TextField(null=True , blank=True)
+    creator       = models.ForeignKey(User)
+    creation_date = models.DateTimeField (auto_now = True, editable = False)
+    deadline      = models.DateTimeField(null=True , blank=True)
+    status        = models.TextField(max_length=50,choices=STAT_CHOICES,default='OPEN')	
+    class Meta:
+        abstract = True
+        
+class Task(AbstractBaseTask):
+    """ Task Model
+
+    Note : The department where the Task originated is understood from
+    the Creator's department. As of now, only Cores can create Tasks.
+
+    A Task mainly consists of SubTasks which are created and assigned by the respective Cores of each department.
+    """
+    class Admin:
+        pass
+
+class SubTask(AbstractBaseTask):
+    """ SubTask Model
+
+    As of now, SubTasks can be created mainly in two places :
+
+    * When a Core breaks Tasks down into SubTasks and assigns some of
+      them to Coords in his own department and/or delegates a SubTask
+      to another Department (whose core has to then assign that
+      SubTask to that department's coords, possibly creating more
+      SubTasks as required)
+
+    * When a Coord creates goals with deadlines for himself (these are
+      called SubTasks but will actually be shown in his Timeline and
+      handled differently from SubTasks that have been assigned to him
+      by Cores)
+    """
+    coords = models.ManyToManyField(User)
+    department = models.ForeignKey (Department)
+    task = models.ForeignKey (Task)
 
     class Admin:
         pass
 
+class AbstractComment (models.Model):
+    """ Abstract Base Class to store a comment.
 
-class Comment (models.Model):
-    """Model to store a comment.
-
-    Timestamp helps to order comments.
+    Timestamp helps to order comments. It is generated automatically.
     Author can be used to select particular comments based on the author.
     """
-
     author = models.ForeignKey (User)
     comment_string = models.TextField ()
     time_stamp = models.DateTimeField (auto_now = True, editable = False)
 
-class label(models.Model):
-    labelname=models.ForeignKey(Task, related_name="task_label")
+    class Meta:
+        abstract = True
+
+class TaskComment(AbstractComment):
+    """ Comment written for a Task.
+    """
+    task = models.ForeignKey (Task)
+        
+class SubTaskComment(AbstractComment):
+    """ Comment written for a SubTask
+    """
+    subtask = models.ForeignKey (SubTask)
+        
+class Label(models.Model):
+    labelname = models.ForeignKey(Task, related_name="task_label")
