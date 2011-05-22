4c4825a068315251a9e1c626df9d8ee59b044d69
Krishna <krishna@Krishna-sonata.(none)>
2011-05-22 15:49:00 +0530

    Commented out the admin part

--- urls.py
+++ urls.py
@@ -19,8 +19,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'erp.views.home', name='home'),
     # url(r'^erp/', include('erp.foo.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
-     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls))
+     #url(r'^admin/', include(admin.site.urls))
 )
 
