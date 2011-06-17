6213b4655133eb12ab1c6ecff420b317148b5193
vivek kumar bagaria <vivekkumarbagaria@ubuntu.(none)>
2011-06-18 02:44:35 +0530

    last commit of the day

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,9 +5,9 @@ from django.contrib import admin
 urlpatterns = patterns('',
       (r'^$', 'erp.tasks.views.display_portal'),
       (r'^home/$', 'erp.tasks.views.display_portal'),
-      (r'^documents/$', 'erp.dashboard.views.documents'),
-      (r'^addteammember/$', 'erp.dashboard.views.addteammember'),
+      (r'^documents/$', 'erp.dashboard.views.details'),
+      (r'^delete_detail/$', 'erp.dashboard.views.delete_otherdetails'),
       (r'^upload_document/$', 'erp.dashboard.views.upload_file'),
       (r'^delete_document/$', 'erp.dashboard.views.delete_file'),
 )
-
+	
--- users/forms.py
+++ users/forms.py
@@ -41,7 +41,6 @@ HOSTEL_CHOICES  =(
    
         )
 #author :vivek kumar bagaria
-#i changed it cause it was not working in the templates
 class AddUserForm (forms.Form):
     username=forms.CharField(max_length=30,help_text='your roll no number is your username')
     email=forms.EmailField(help_text='Enter your e-mail address. eg, someone@gmail.com')
@@ -117,7 +116,10 @@ class personal_details(ModelForm):
 	model=userprofile
 	exclude=('user',)
 
-
+class OtherContactDetails_form(ModelForm):
+    class Meta:
+        model=OtherContactDetails
+        exclude=('user',)
 #author: vivek
 class InviteForm (ModelForm):
     class Meta:
