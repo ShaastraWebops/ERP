7e69b4bb6956b28bf1a4cbefe2879e475e088951
Swapnil Basak <swapnilbasak@gmail.com>
2012-07-17 01:52:51 +0530

    Added search as a separate app

---
+++ search/views.py
@@ -0,0 +1,41 @@
+# Create your views here.
+
+from haystack.query import *
+from erp.tasks.models import *
+from erp.users.models import *
+from erp.search.wrappers import *
+from django.shortcuts import render_to_response, HttpResponseRedirect
+from django.template import RequestContext
+import urllib
+from django.template.context import Context, RequestContext
+from erp.misc.helper import is_core, is_coord, get_page_owner
+from django.template.context import Context, RequestContext
+from django.contrib.auth.decorators import login_required
+from erp.misc.util import *
+
+
+@login_required(login_url='/erp/', redirect_field_name=None)
+def search(request, search_term=None):  
+    page_owner = get_page_owner (request, None)                               
+    if request.method == "POST":
+        query = request.POST['searchbar']
+        search_term = '/search/'+str(urllib.urlencode({'':query}))
+        print search_term
+        return HttpResponseRedirect(search_term)         
+    if not search_term:
+            return render_to_response('search/search.html', locals(),RequestContext(request))     
+    results = handle_search(request,search_term)
+    spelling_suggestion = spelling(search_term)
+    print results
+    print spelling_suggestion
+    tasks = results['tasks']
+    users = results['users']
+    return render_to_response('search/search.html', locals(),RequestContext(request))
+            
+    
+     
+    
+            
+
+        
+    
---
+++ search/wrappers.py
@@ -0,0 +1,16 @@
+from haystack.query import *
+from erp.tasks.models import *
+from erp.users.models import *
+
+
+def handle_search(request,search_term):
+    search_results = SearchQuerySet()
+    tasks = search_results.filter(content = search_term).models(Task)
+    users = search_results.filter(content = search_term).models(userprofile)
+    subtasks = search_results.filter(content = search_term).models(SubTask)
+    results = {'tasks':tasks, 'users':users }
+    return results
+    
+def spelling(search_term):
+
+    return SearchQuerySet().spelling_suggestion(search_term)
