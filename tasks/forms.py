641d00db1a597950e8d3cca6416f78106c3186be
Swapnil Basak <swapnilbasak@gmail.com>
2012-07-19 03:47:23 +0530

    formatting modelforms

--- tasks/forms.py
+++ tasks/forms.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.forms import ModelForm
 from models import *
 from django import forms
+from chosen import forms as chosenforms
+from chosen import widgets as chosenwidgets
+
 class TaskCommentForm (ModelForm):
     class Meta:
         model = TaskComment
         exclude = ('author', 'task')
+        
 
 class SubTaskCommentForm (ModelForm):
     class Meta:
@@ -21,8 +25,16 @@ class TaskForm (ModelForm):
     class Meta:
         model = Task
         exclude = ('creator', )
+        
 
 class SubTaskForm (ModelForm):
     class Meta:
         model = SubTask
-        exclude = ('creator', 'description', 'department', 'task')
+        exclude = ['creator', 'description', 'department', 'task']
+
+#hack if these fields need to be formatted        
+#    def __init__(self, *args, **kwargs):
+#        super(SubTaskForm, self).__init__(*args, **kwargs)
+#        self.fields['subject'].widget.attrs['rows'] = '500'
+#        self.fields['subject'].widget.attrs['cols'] = '500'
+        
