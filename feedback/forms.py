50ba4576518db31544c4f37fc6761261c0efeb4d
Swaroop Ramaswamy <swaroop551992@gmail.com>
2012-07-20 01:03:44 +0530

    Indented feedback code

--- feedback/forms.py
+++ feedback/forms.py
@@ -6,28 +6,28 @@ from django import forms
 from django.utils.safestring import mark_safe
 
 class HorizontalRadioRenderer(forms.RadioSelect.renderer):
-  def render(self):
-    return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
+    def render(self):
+        return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
 
 RATING_CHOICES= [(i,i) for i in range(11)]
 
 
 class QuestionFormCoord (ModelForm):
     class Meta:
-		model = Question
-		fields= ('question','answered_by','departments',)
+        model = Question
+        fields= ('question','answered_by','departments',)
 
 
 class QuestionFormCore (ModelForm):
     class Meta:
-		model = Question
-		fields= ('question','departments',)
-        
+        model = Question
+        fields= ('question','departments',)
+
 class AnswerForm (ModelForm):
     rating = forms.ChoiceField(choices=RATING_CHOICES,widget=forms.RadioSelect)
     class Meta:
         model = Answer
         fields = ('rating',)
 
-        
-        
+
+
--- feedback/models.py
+++ feedback/models.py
@@ -6,41 +6,41 @@ from erp.users.models import userprofile
 from erp.misc.helper import is_core, is_coord, get_page_owner
 
 STAT_CHOICES= (
-    ('Core','Core'),
-    ('Coord','Coordinator'),
-    ('Vol','Volunteer'),
-    ('All','All'),
-)
+        ('Core','Core'),
+        ('Coord','Coordinator'),
+        ('Vol','Volunteer'),
+        ('All','All'),
+        )
 
 FOR_CHOICES=(
-	('Core','Core')
-	('Coord',
+        ('Core','Core')
+        ('Coord',
 
-class Question(models.Model):
-    question = models.CharField(max_length=200)
-    departments = models.ManyToManyField(Department)
-    answered_by = models.CharField(max_length=5,choices=STAT_CHOICES,default = 'All')
-    creator=models.ForeignKey(userprofile,related_name='question_creator',blank=True,null=True)
-	feedback_for=models.CharField(max_length=5,choices=FOR_CHOICES,blank=True,null=True)
-    def __str__(self):
-        return self.question
-        
-class Answer(models.Model):
-    question = models.ForeignKey(Question)
-    owner = models.ForeignKey(userprofile, related_name='answer_owner')
-    creator = models.ForeignKey(userprofile, related_name='answer_creator')
-    rating = models.IntegerField(blank=True,null=True)
-    answered = models.BooleanField(default=False)
-    submitted = models.BooleanField(default=False)
+            class Question(models.Model):
+            question = models.CharField(max_length=200)
+            departments = models.ManyToManyField(Department)
+            answered_by = models.CharField(max_length=5,choices=STAT_CHOICES,default = 'All')
+            creator=models.ForeignKey(userprofile,related_name='question_creator',blank=True,null=True)
+            feedback_for=models.CharField(max_length=5,choices=FOR_CHOICES,blank=True,null=True)
+            def __str__(self):
+            return self.question
 
-    def __str__(self):
-        return self.rating      
-        
-class Answeravg(models.Model):
-    question = models.ForeignKey(Question)
-    owner = models.ForeignKey(userprofile)
-    num = models.DecimalField(max_digits=6, decimal_places=2)
-    avg = models.DecimalField(max_digits=6, decimal_places=2)
+            class Answer(models.Model):
+            question = models.ForeignKey(Question)
+            owner = models.ForeignKey(userprofile, related_name='answer_owner')
+            creator = models.ForeignKey(userprofile, related_name='answer_creator')
+            rating = models.IntegerField(blank=True,null=True)
+            answered = models.BooleanField(default=False)
+            submitted = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.avg
+            def __str__(self):
+            return self.rating      
+
+            class Answeravg(models.Model):
+            question = models.ForeignKey(Question)
+            owner = models.ForeignKey(userprofile)
+            num = models.DecimalField(max_digits=6, decimal_places=2)
+            avg = models.DecimalField(max_digits=6, decimal_places=2)
+
+            def __str__(self):
+            return self.avg
