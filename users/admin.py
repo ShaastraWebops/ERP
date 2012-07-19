fe0fe4a5bd7c1268c212fe8bed3e667412d3a21b
Janarthanan R <rsdjjana@gmail.com>
2012-07-20 02:27:28 +0530

    font size bug solved(number:10)

--- users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from erp.users.models import *
-
-admin.site.register(userprofile)
-admin.site.register(invitation)
-admin.site.register(Materials)
-admin.site.register(userphoto)
