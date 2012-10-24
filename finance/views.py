8aaf897bca4216de72c221bb108bd05279c8a713
Janarthanan R <rsdjjana@gmail.com>
2012-10-24 23:06:57 +0530

    bug in item delete solved

--- finance/views.py
+++ finance/views.py
@@ -152,7 +152,7 @@ def budget_portal(request, plan="None"):
                                     tempform.budget=curr_plan
                                     tempform.save()
                                 if form in itemformset.deleted_forms:
-                                    if item.objects.filter(id=form.instance.id):
+                                    if Item.objects.filter(id=form.instance.id):
                                         curr_item = Item.objects.get(id=form.instance.id)
                                         curr_item.delete()   
                             form_saved = True 
