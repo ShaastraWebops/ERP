6f757905be288c3d5360a89785fafec263bb57e6
Swapnil Basak <swapnilbasak@gmail.com>
2012-12-14 01:24:13 +0530

    Some advancements

--- prizes/urls.py
+++ prizes/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.conf.urls import *
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('erp.prizes.views',
+urlpatterns = patterns('erp.prizes.views',     
+      (r'^assign/$', 'prize_assign'),
       (r'^$', 'prize_details'),   
 )
 
--- prizes/views.py
+++ prizes/views.py
@@ -8,4 +8,8 @@ from erp.prizes.models import Prize
 def prize_details(request,owner_name=None):
     prizes=Prize.objects.filter(event=request.user.get_profile ().department)
     return render_to_response('prizes/prize_base.html',locals(),context_instance=global_context(request))
-    
+
+def prize_assign(request,owner_name=None):
+    prizes=Prize.objects.filter(event=request.user.get_profile ().department)
+    return render_to_response('prizes/prize_assign.html',locals(),context_instance=global_context(request))
+
