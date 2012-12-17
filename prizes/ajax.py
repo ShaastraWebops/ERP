942e9f41f46694f504e5efc7a510cdb30a63f1e0
Swapnil Basak <swapnilbasak@gmail.com>
2012-12-18 03:51:25 +0530

    Assigning cheques

--- prizes/ajax.py
+++ prizes/ajax.py
@@ -11,8 +11,21 @@ def submit(request,name=None,position=None,details=None):
     if not new.cheque:
         new.cheque='<Not Assigned>'
     new.save()    
-    prizes=Prize.objects.all()   
+    prizes=Prize.objects.all().order_by('-pk')
     template=get_template('prizes/prize_table.html')
     markup=template.render(Context({'prizes':prizes}))
     dajax.assign('#prize_table','innerHTML',markup)
+    return dajax.json()
+    
+@dajaxice_register
+def assign(request,id=None,serial=None):
+    dajax=Dajax()
+    print "coming here", id, serial
+    p=Prize.objects.filter(pk=id)[0]
+    p.cheque=serial
+    p.save()
+    prizes=Prize.objects.all()
+    template=get_template('prizes/assign_table.html')
+    markup=template.render(Context({'prizes':prizes}))
+    dajax.assign('#assign_table','innerHTML',markup)
     return dajax.json()
--- prizes/views.py
+++ prizes/views.py
@@ -6,7 +6,7 @@ from erp.misc.util import *
 from erp.prizes.models import Prize
 
 def prize_details(request,owner_name=None):
-    prizes=Prize.objects.filter(event=request.user.get_profile ().department)
+    prizes=Prize.objects.filter(event=request.user.get_profile ().department).order_by('-pk')
     return render_to_response('prizes/prize_base.html',locals(),context_instance=global_context(request))
 
 def prize_assign(request,owner_name=None):
