78e4e366378a346a85af8195d3d9b3ee88bada97
Shaastra <webops@shaastra.org>
2013-01-04 03:07:06 +0530

    Not sure what was wrong

--- prizes/dbcopy.py
+++ prizes/dbcopy.py
@@ -26,33 +26,34 @@ def fullcopy():
 import time
 
 def check_and_copy():
-    f=open('prizes/pk.txt','rb')
-    x=int(f.read())
-    users=ParticipantUser.objects.using('main').order_by('pk')
-    length=len(users)
-    print length-x, "new instances copied"
-    if x-length!=0:
-        for user in users[(x-1):(length-1)]:
-            try:
-                erp_user = Participant() 
-                erp_user.name = user.user.first_name + user.user.last_name
-                erp_user.gender = user.gender
-                erp_user.branch = user.branch
+
+    while True:
+        f=open('prizes/pk.txt','rb')
+        x=int(f.read())
+        users=ParticipantUser.objects.using('main').order_by('pk')
+        length=len(users)
+        print length-x, "new instances copied"
+        if x-length!=0:
+            for user in users[(x-1):(length-1)]:
                 try:
-                    erp_user.college = user.college.name
+                    erp_user = Participant() 
+                    erp_user.name = user.user.first_name + user.user.last_name
+                    erp_user.gender = user.gender
+                    erp_user.branch = user.branch
+                    try:
+                        erp_user.college = user.college.name
+                    except:
+                        pass
+                    erp_user.mobile_number = user.mobile_number
+                    erp_user.shaastra_id = user.shaastra_id
+                    erp_user.college_roll = user.college_roll
+                    erp_user.branch = user.branch
+                    erp_user.save()
+                    print "copied",user.pk
                 except:
-                    pass
-                erp_user.mobile_number = user.mobile_number
-                erp_user.shaastra_id = user.shaastra_id
-                erp_user.college_roll = user.college_roll
-                erp_user.branch = user.branch
-                erp_user.save()
-                print "copied",user.pk
-            except:
-                print 'did not save', user.pk
-    f=open('prizes/pk.txt','w')
-    f.write(str(length))
-    f.close()
-    time.sleep(2)
-    print "Run check_and_copy delay-2"
-    check_and_copy()
+                    print 'did not save', user.pk
+        f=open('prizes/pk.txt','w')
+        f.write(str(length))
+        f.close()
+        time.sleep(2)
+        print "Run check_and_copy delay-2"
